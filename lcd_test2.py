da62c37da1d32b4e0b53902ebec1f8bbe3e3058c
provwise <provwise@gmail.com>
2019-04-23 23:44:52 -0400

    Now includes function for enabling/disabling fan in test cycle

--- lcd_test2.py
+++ lcd_test2.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 
 import sensor_functions as sensor
+import RPi.GPIO as PINS
 
 while True:
 
   try:
     sensor.temp_update()
+    sensor.fan_check()
     sensor.get_distance()
 
   except:
     sensor.lcd_shutdown()
+    PINS.cleanup()
     break
+
