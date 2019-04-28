23824e310a869ea5e8732764163cc045ef1de5d5
provwise <provwise@gmail.com>
2019-04-28 15:37:43 -0400

    Added file to turn fan on or off based on temp/humidity readings

---
+++ fan_check.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from time import *
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+from libraries import relay
+
+sensor = Adafruit_DHT.DHT22
+pin = 22
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+sleep(2)
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+if humidity is not None and temperature is not None:
+    humidity = float(humidity)
+    temperature = float(temperature)
+
+    if humidity > 20.0 or temperature > 24.0:
+        relay.relay1_off()
+    
+    else: 
+        relay.relay1_on()
+
+else:
+    pass
